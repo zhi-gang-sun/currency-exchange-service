3e417c8cd722dd609582b3beed092870bb3980c1
owen <zgsun@cn.ibm.com>
2021-04-12 17:41:31 +0800

    add zipkin

--- src/main/java/com/ibm/cio/dsw/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/ibm/cio/dsw/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.ibm.cio.dsw.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,8 @@ public class CurrencyExchangeController {
 	@Autowired
 	private Environment environment;
 	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchange.class);
+	
 	@Autowired
 	private CurrencyExchangeRepository repo;
 
@@ -21,6 +25,8 @@ public class CurrencyExchangeController {
 	public CurrencyExchange exchange(@PathVariable String from, @PathVariable String to) throws RuntimeException {
 
 		//CurrencyExchange currencyExchange = new CurrencyExchange(1L, from, to, BigDecimal.valueOf(6.50));
+		
+		logger.info("exchange called with {} to {}", from, to );
 		
 		CurrencyExchange currencyExchange = repo.findByFromAndTo(from, to);
 		
